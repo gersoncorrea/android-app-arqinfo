1144ea435b0100ab54834f6880d4da3dce62d5ae
gersoncorrea <gersoncorrea.jr@gmail.com>
2022-05-07 10:58:52 -0400

    criacao de objeto para teste

---
+++ feature-authentication/src/test/java/com/app/feature/authentication/login/TestObjects.kt
@@ -0,0 +1,29 @@
+package com.app.feature.authentication.login
+
+import com.app.feature.authentication.domain.LabelsDefault
+import com.app.feature.authentication.domain.LoginBottom
+import com.app.feature.authentication.domain.LoginHeader
+import com.app.feature.authentication.domain.LoginModel
+
+object TestObjects {
+
+    val loginObject = LoginModel(
+        header = LoginHeader(
+            img = "imglink",
+            labelUserInput = LabelsDefault(
+                title = "title",
+                type = "type"
+            ),
+            labelPasswordInput = LabelsDefault(
+                title = "title",
+                type = "type"
+            ),
+        ),
+        forgotPassword = "forgotPassword",
+        bottom = LoginBottom(
+            button = "button",
+            signup = "signup"
+        )
+    )
+
+}
